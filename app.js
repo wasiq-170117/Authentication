3bb269470cd90b37100671fe65841706a18fe143
Wasiq Sheikh <wasiqsheikh1998@gmail.com>
2023-02-18 21:40:31 +0500

    Level 6 Oauth 2.0 with Google Sign in Strategy

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+
 
 
 //const encrypt = require('mongoose-encryption');
@@ -35,11 +38,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 const userschema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userschema.plugin(passportLocalMongoose);
-
+userschema.plugin(findOrCreate);
 
 
 //userschema.plugin(encrypt, {secret: process.env.SECRETKEY, encryptedFields: ["password"]}); 
@@ -48,8 +52,36 @@ const User = mongoose.model("User", userschema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+//Used for passport-local-mongoose//
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+//Used for passport-local-mongoose//
+
+//Used for and Local Strategies//
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    })
+});
+//Used for and Local Strategies//
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log('profile = ', profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 
 mongoose.set('strictQuery', false);
 mongoose.connect("mongodb://localhost:27017/userDb");
@@ -57,6 +89,17 @@ mongoose.connect("mongodb://localhost:27017/userDb");
 app.get("/", function(req, res) {
     res.render("home");
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+);
+
+app.get("/auth/google/secrets", 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect Secrets.
+    res.redirect('/secrets');
+  });
 
 app.get("/register", function(req, res) {
     res.render("register");
